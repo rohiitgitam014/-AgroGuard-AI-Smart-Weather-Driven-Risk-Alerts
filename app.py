c1d64ad776fc47d717dcf2bd10994bed36fec31e
rohiitgitam014 <145531927+rohiitgitam014@users.noreply.github.com>
2025-07-24 16:14:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ st.markdown("Get real-time agricultural risk alerts based on live weather condit
 
 # --- Get Real-time Weather Data from NASA API ---
 def get_weather_data(lat, lon):
+    yesterday = (datetime.datetime.today() - datetime.timedelta(days=1)).strftime('%Y%m%d')
     url = f"https://power.larc.nasa.gov/api/temporal/daily/point?parameters=T2M,RH2M,WS2M,PRECTOTCORR&community=AG&longitude={lon}&latitude={lat}&start={yesterday}&end={yesterday}&format=JSON"
 
     
